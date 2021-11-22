ae02e8be19d6fbebea5e9af4c9f0d2ab076896e7
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-11-22 12:18:26 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,39 @@
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+import serial
+import time
+import subprocess
+import decimal
+
+class ucfjokeSkill(MycroftSkill):
+
+    def __init__(self):
+        super().__init__()
+        self.learning = True
+
+    def initialize(self):
+        my_setting = self.settings.get('my_setting')
+
+    @intent_handler('ucfjoke.intent')
+    def handle_not_are_you_intent(self, message):
+        self.speak_dialog("Whats the difference between me and a you es ef student")
+        serA = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        serA.flush()
+        serA.write(b"joke")
+        serB = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+        serB.flush()
+        serB.write(b"joke")        
+        serC = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        serC.flush()
+        serC.write(b"joke")
+        serD = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        serD.flush()
+        serD.write(b"joke")
+        time.sleep(2)
+        self.speak_dialog("Nothing we were both born yesterday")
+
+    def stop(self):
+        pass
+
+def create_skill():
+    return ucfjokeSkill()
